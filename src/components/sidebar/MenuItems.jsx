92f4d8484db0cd1ec0a5888add5af23c60451c18
SANJAY-1508 <psanjaykumar377@gmail.com>
2025-11-07 11:14:42 +0530

    `Removed various components, services, and slices related to company payroll, staff, and other features`

--- src/components/sidebar/MenuItems.jsx
+++ src/components/sidebar/MenuItems.jsx
@@ -1,110 +1,17 @@
 import { MdOutlineHome } from "react-icons/md";
-import { MdAdjust } from "react-icons/md";
 import { BiUserPlus } from "react-icons/bi";
-import { BsFilePlus } from "react-icons/bs";
-import { LiaFanSolid } from "react-icons/lia";
-import { GiRopeDart } from "react-icons/gi";
-import { BsBox } from "react-icons/bs";
-import { BsBoxes } from "react-icons/bs";
-import { GiPunch } from "react-icons/gi";
-import { BiSolidReport } from "react-icons/bi";
-import { GiSkippingRope } from "react-icons/gi";
-import { GrRestroomMen } from "react-icons/gr";
-import { GiExplosiveMeeting } from "react-icons/gi";
+
 const MenuItems = [
+  {
+    path: "/dashboard",
+    text: "Dashboard",
+    icon: <MdOutlineHome />,
+  },
   // {
-  //   path: "/dashboard",
-  //   text: "Dashboard",
-  //   icon: <MdOutlineHome />,
+  //   path: "/users",
+  //   text: "Users",
+  //   icon: <BiUserPlus />,
   // },
-  {
-    path: "/users",
-    text: "Users",
-    icon: <BiUserPlus />,
-  },
-  {
-    path: "/master/category",
-    text: "Master",
-    icon: <BsFilePlus />,
-    submenu: [
-      // {
-      //   path: "/master/group",
-      //   text: "Group",
-      // },
-      // {
-      //   path: "/master/category/",
-      //   text: "Category",
-      // },
-      {
-        path: "/master/products/",
-        text: "பொருள்கள்",
-      },
-      {
-        path: "/master/Staff/",
-        text: "ஊழியர்கள்",
-      },
-      {
-        path: "/master/Ringpunch",
-        text: "வளையம் குத்து",
-      },
-      {
-        path: "/master/paysetting",
-        text: "செலுத்து ஷெட்டினக்ஸ்",
-      },
-    ],
-  },
-
-  // {
-  //   path: "/plateentry",
-  //   text: "Plate Entry",
-  //   icon: <MdAdjust />,
-  // },
-  {
-    path: "/Ringboxingsection",
-    text: "வளையம் குத்து பிரிவு",
-    icon: <LiaFanSolid />,
-  },
-  {
-    path: "/knotting",
-    text: "பின்னல் பிரிவு",
-    icon: <GiRopeDart />,
-  },
-  {
-    path: "/deluxe",
-    text: "டீலக்ஸ் பின்னல்",
-    icon: <GiSkippingRope />,
-  },
-  {
-    path: "/packing",
-    text: "பாக்கெட் பிரிவு",
-    icon: <BsBox />,
-  },
-  {
-    path: "/company",
-    text: " கம்பெனி பிரிவு",
-    icon: <BsBoxes />,
-  },
-  {
-    path: "/pay",
-    text: "செலுத்து பிரிவு",
-    icon: <GiPunch />,
-  },
-  {
-    path: "/Explosivedevice",
-    text: "வெடி உருடு பிரிவு",
-    icon: <GiExplosiveMeeting />,
-  },
-
-  {
-    path: "/report",
-    text: "Report",
-    icon: <BiSolidReport />,
-  },
-  {
-    path: "/companyreport",
-    text: "CompanyReport",
-    icon: <GrRestroomMen />,
-  },
 ];
 
 export default MenuItems;
--- src/view/listings/DashBoard.jsx
+++ src/view/listings/DashBoard.jsx
@@ -1,79 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
-import Stock from "../listings/stocks/Stock";
-import Plate from "./stocks/Plates";
-import { Buttons } from "../../components/Buttons";
-
-import Knotting from "./stocks/Knotting";
-import Packing from "./stocks/Packing";
 
 const DashBoard = () => {
-  const [activeComponent, setActiveComponent] = useState("Stock");
-
-  const handleButtonClick = (component) => {
-    setActiveComponent(component);
-  };
-
   return (
     <div id="main">
       <Container>
         <Row>
-          <Col xs="12">
-            <ul className="d-flex mb-5 border-bottom">
-              <li className="mx-3">
-                <Buttons
-                  btnlabel="Dashboard"
-                  className={`tab-btn ${
-                    activeComponent === "Stock" ? "bactive" : "cactive"
-                  }`}
-                  onClick={() => handleButtonClick("Stock")}
-                />
-              </li>
-              {/* <li className="mx-3">
-                  <Buttons
-                    btnlabel="Plate"
-                    className={`tab-btn  ${
-                      activeComponent === "Plate" ? "bactive" : "cactive"
-                    }`}
-                    onClick={() => handleButtonClick("Plate")}
-                  />
-                </li>
-                <li className="mx-3">
-                  <Buttons
-                    btnlabel="Dry Process"
-                    className={`tab-btn  ${
-                      activeComponent === "DryProcess" ? "bactive" : "cactive"
-                    }`}
-                    onClick={() => handleButtonClick("DryProcess")}
-                  />
-                </li>
-                <li className="mx-3">
-                  <Buttons
-                    btnlabel="Knotting"
-                    className={`tab-btn  ${
-                      activeComponent === "Knotting" ? "bactive" : "cactive"
-                    }`}
-                    onClick={() => handleButtonClick("Knotting")}
-                  />
-                </li>
-                <li className="mx-3">
-                  <Buttons
-                    btnlabel="Packing"
-                    className={`tab-btn  ${
-                      activeComponent === "Packing" ? "bactive" : "cactive"
-                    }`}
-                    onClick={() => handleButtonClick("Packing")}
-                  />
-                </li> */}
-            </ul>
-          </Col>
+          <Col xl="12"></Col>
         </Row>
       </Container>
-      {activeComponent === "Stock" && <Stock />}
-      {/* {activeComponent === "Plate" && <Plate />}
-      {activeComponent === "DryProcess" && <DryProcess />}
-      {activeComponent === "Knotting" && <Knotting />}
-      {activeComponent === "Packing" && <Packing />} */}
     </div>
   );
 };
